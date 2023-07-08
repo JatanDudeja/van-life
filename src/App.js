8dbc2290f750075c8202698c8e6859ae52738afe
Jatan Dudeja <dudeja.jatan04@gmail.com>
2023-07-09 00:30:13 +0530

    some addition and p tag fix in home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import Home from '../src/components/Home'
 import About from '../src/components/About'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Vans from '../src/components/Vans'
+import VansDetails from '../src/components/VansDetails'
+
+import './server'
 
 export default function App(){
   return (
@@ -10,11 +14,14 @@ export default function App(){
         <Link className = 'van-life' to='/'>#VanLife</Link>
         <nav className='nav-bar'>
           <Link className = 'about-us-link'to = '/about'>AboutUs</Link>
+          <Link className='about-us-link' to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
       <Route path = "/" element = {<Home/>}/>
       <Route path = '/about' element = {<About/>}/>
+      <Route path = '/vans' element = {<Vans/>}/>
+      <Route path = '/vans/:id' element = {<VansDetails/>}/>
       </Routes>
       <footer><p><span></span>©2023 #VANSLIFE</p></footer>
     </BrowserRouter>
